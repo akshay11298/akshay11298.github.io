f1c5ac40f11e3dc96242c32a221c9d9aadfb611d
akshay11298 <akshay.avinash@gmail.com>
2017-06-08 21:37:58 +0530

    fonts added

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device=width , initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Barrio' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Indie Flower' rel='stylesheet'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
@@ -16,7 +18,7 @@
     <div class="col-sm-3">Hello Earthlings! </div>
     <ul class="nav navbar-nav navbar-right">
         <li><a href="#about">About me!</a></li>
-        <li><a href="#">Education</a></li>
+        <li><a href="#education">Education</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Contact me !!</a></li>
     </ul>
@@ -28,7 +30,7 @@
 <div id="about" class="container-fluid">
     <div class="row slideanim">
         <div class="col-sm-8">
-            <div class="text-center"><h2>about me!</h2></div>
+            <div class="indie"><h2>about me!</h2></div>
             <div class="me">
                 <h3>I am a computer science student</h3>
                 Hi fellas! I am a computer science student studying in SRM University. Currently I'm in third year. Well that's enough about the boring stuff :P<br>
@@ -47,7 +49,7 @@
                 <!--<li data-target="#myCarousel" data-slide-to="5"></li>
                 <li data-target="#myCarousel" data-slide-to="6"></li>-->
             </ol>
-            <div class="carousel-inner" role="listbox">
+            <div class="carousel-inner indie" role="listbox">
                 <div class="item active">
                     <img src="img/a.jpg" class="img-rounded img-responsive">
                     <div class="carousel-caption text-center">
@@ -90,6 +92,39 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
+        </div>
+    </div>
+</div>
+<div id="education" class="container-fluid">
+    <div class="row slideanim">
+        <div class="col-sm-4">
+            <div id="childhood" class="carousel text-center" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#childhood" data-slide-to="0" class="active"></li>
+                    <li data-target="#childhood" data-slide-to="1"></li>
+                    <li data-target="#childhood" data-slide-to="1"></li>
+                    <li data-target="#childhood" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner indie" role="listbox">
+                    <div class="item active">
+                        <img src="img/child.jpeg" class="img-rounded">
+                        <div class="carousel-caption">
+                            <h2>Cutie ?</h2>
+                            <p> maybe :P</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="indie"><h2>education !</h2></div>
+            I started my schooling in this wonderful school, Young Folks Academy, in Gorakhpur,Uttar Pradesh. It was the base of my education and it disciplined me. But I had to change because my father
+            had a transferable job.<br>
+            I moved out in 3<sup>rd</sup> standard and joined Amarvani School, which was in Orissa. It was a missionary school and so strictness was implied.  When I was in 6<sup>th</sup> standard,
+            I joined Maxfort School, which is in New Delhi, and completed my schooling from here. This was the school which matured me with utmost love and care. It helped me to groom into the man I am
+            today. I completed my 10<sup>th</sup> &amp 12<sup>th</sup>in this school.<br>
+            My 10<sup>th</sup> score was <em><strong>9.8</strong></em>
+
         </div>
     </div>
 </div>
